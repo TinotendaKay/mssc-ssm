f8dbd7caed5e1245ac305701c4385f8142182608
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2021-10-19 01:18:06 +0200

    2nd commit

---
+++ src/test/java/guru/framework/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
